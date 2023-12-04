d29438e9946dbdec65314116094a163d9f304d28
francomattar <mattarfranco3@gmail.com>
2023-12-04 23:02:13 +0600

    initialize redux user persistance

---
+++ client/components/redux/Persistance.tsx
@@ -0,0 +1,24 @@
+/**
+ * Title: Write a program using JavaScript on Persistance
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 04, December 2023
+ */
+
+"use client";
+
+import React, { ReactNode } from "react";
+
+const Persistance = ({ children }: { children: ReactNode }) => {
+  return <> {children} </>;
+};
+
+export default Persistance;
