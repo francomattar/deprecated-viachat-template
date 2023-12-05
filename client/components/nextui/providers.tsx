c03a00c4d7e6ef1215112ad6ffbc224fda02b865
francomattar <mattarfranco3@gmail.com>
2023-12-05 19:13:24 +0600

    initialize next ui provider

---
+++ client/components/nextui/providers.tsx
@@ -0,0 +1,23 @@
+/**
+ * Title: Write a program using JavaScript on Providers
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 05, December 2023
+ */
+
+
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
