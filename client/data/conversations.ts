890aa9aef91a3cacc08028e8a6595dc4a74e4301
francomattar <mattarfranco3@gmail.com>
2023-12-07 09:44:12 +0600

    modify dummy conversation data

--- client/data/conversations.ts
+++ client/data/conversations.ts
@@ -1,127 +1,55 @@
 const conversations = [
   {
     _id: 1,
-    messages: [
-      {
-        _id: 1,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
+    sender: [
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     ],
+    receiver: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit."],
     createdAt: "2020-01-01T00:00:00.000Z",
   },
   {
     _id: 2,
-    messages: [
-      {
-        _id: 2,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
-      {
-        _id: 3,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
+    sender: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit."],
+    receiver: [
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     ],
     createdAt: "2020-01-01T00:00:00.000Z",
   },
   {
     _id: 3,
-    messages: [
-      {
-        _id: 3,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
+    sender: [
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    ],
+    receiver: [
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     ],
     createdAt: "2020-01-01T00:00:00.000Z",
   },
   {
     _id: 4,
-    messages: [
-      {
-        _id: 4,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
+    sender: [
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     ],
-    createdAt: "2020-01-01T00:00:00.000Z",
-  },
-  {
-    _id: 5,
-    messages: [
-      {
-        _id: 5,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
-    ],
-    createdAt: "2020-01-01T00:00:00.000Z",
-  },
-  {
-    _id: 6,
-    messages: [
-      {
-        _id: 6,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
-    ],
-    createdAt: "2020-01-01T00:00:00.000Z",
-  },
-  {
-    _id: 7,
-    messages: [
-      {
-        _id: 7,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
-    ],
-    createdAt: "2020-01-01T00:00:00.000Z",
-  },
-  {
-    _id: 8,
-    messages: [
-      {
-        _id: 8,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
-    ],
-    createdAt: "2020-01-01T00:00:00.000Z",
-  },
-  {
-    _id: 9,
-    messages: [
-      {
-        _id: 9,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
-    ],
-    createdAt: "2020-01-01T00:00:00.000Z",
-  },
-  {
-    _id: 10,
-    messages: [
-      {
-        _id: 10,
-        sender: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        receiver: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        createdAt: "2020-01-01T00:00:00.000Z",
-      },
+    receiver: [
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
     ],
     createdAt: "2020-01-01T00:00:00.000Z",
   },
