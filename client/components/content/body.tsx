f18a1de6f3a08e641699dc10cd5fbb675db071a7
francomattar <mattarfranco3@gmail.com>
2023-12-07 09:56:38 +0600

    modify conversation with their owner avatar

--- client/components/content/body.tsx
+++ client/components/content/body.tsx
@@ -14,7 +14,7 @@
  */
 
 import conversations from "@/data/conversations";
-import { ScrollShadow } from "@nextui-org/react";
+import { Avatar, ScrollShadow } from "@nextui-org/react";
 import React from "react";
 
 const Body = () => {
@@ -24,23 +24,50 @@ const Body = () => {
       className="h-full border rounded-lg p-3 overflow-y-auto"
     >
       {conversations?.map((conversation) => (
-        <div key={conversation?._id} className="flex flex-col gap-y-2.5">
-          {conversation?.receiver.map((message) => (
-            <p
-              key={message}
-              className="w-[45%] mr-auto bg-[#705ad3] p-2 rounded-md text-sm"
-            >
-              {message}
-            </p>
-          ))}
-          {conversation?.sender.map((message) => (
-            <p
-              key={message}
-              className="w-[45%] ml-auto bg-[#705ad3] p-2 rounded-md text-sm"
-            >
-              {message}
-            </p>
-          ))}
+        <div key={conversation?._id} className="flex flex-col gap-y-4">
+          <div />
+
+          {/* Receiver's Messages */}
+          <div className="w-[45%] mr-auto flex flex-row items-end gap-x-2">
+            <div>
+              <Avatar
+                src="https://i.pravatar.cc/50"
+                className="h-[30px] w-[30px] rounded-full"
+              />
+            </div>
+            <div className="flex flex-col gap-y-2.5">
+              {conversation?.receiver.map((message) => (
+                <p
+                  key={message}
+                  className="bg-[#705ad3] p-2 rounded-md text-sm"
+                >
+                  {message}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          {/* Sender's Messages */}
+          <div className="w-[45%] ml-auto flex flex-row items-end gap-x-2">
+            <div className="order-2">
+              <Avatar
+                src="https://i.pravatar.cc/50"
+                className="h-[30px] w-[30px] rounded-full"
+              />
+            </div>
+            <div className="flex flex-col gap-y-2.5 order-1">
+              {conversation?.sender.map((message) => (
+                <p
+                  key={message}
+                  className="bg-[#705ad3] p-2 rounded-md text-sm"
+                >
+                  {message}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <div />
         </div>
       ))}
     </ScrollShadow>
