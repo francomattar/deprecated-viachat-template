8169360b5c5d75d05c8ac39dcc4d8fdac5fadcf0
francomattar <mattarfranco3@gmail.com>
2023-12-17 20:55:28 +0600

    integrate mobile menu for sidebar

--- client/components/header/navbar.tsx
+++ client/components/header/navbar.tsx
@@ -13,14 +13,45 @@
  * Date: 05, December 2023
  */
 
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import { IoLogOutOutline } from "react-icons/io5";
-import { Avatar, Tooltip } from "@nextui-org/react";
+import { Avatar, Chip, Tooltip } from "@nextui-org/react";
+import { FiMenu } from "react-icons/fi";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  collapseSidebar,
+  expandSidebar,
+} from "@/features/sidebar/sidebarSlice";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = (): React.ReactNode => {
+  const sidebar = useSelector(
+    (state: { sidebar: { expand: boolean; collapse: boolean } }) =>
+      state.sidebar
+  );
+  const dispatch = useDispatch();
+
   return (
     <section className="flex flex-row justify-between items-center">
+      {/* Sidebar Section */}
+      <span className="text-white border-2 rounded-full p-1.5 md:hidden">
+        {sidebar.collapse && (
+          <FiMenu
+            className="h-4 w-4"
+            onClick={() => dispatch(expandSidebar())}
+          />
+        )}
+        {sidebar.expand && (
+          <RxCross2
+            className="h-4 w-4"
+            onClick={() => dispatch(collapseSidebar())}
+          />
+        )}
+      </span>
+
       {/* Logo Section */}
       <Image src="/logo.svg" alt="logo" height={30} width={30} />
 
@@ -36,13 +67,12 @@ const Navbar = (): React.ReactNode => {
           ],
         }}
       >
-        <button className="flex flex-row items-center gap-2 border rounded-full p-0.5">
-          <Avatar
-            src="https://i.pravatar.cc/50"
-            className="h-[20px] w-[20px] rounded-full"
-          />
-          <IoLogOutOutline className="w-5 h-5" />
-        </button>
+        <Chip
+          variant="bordered"
+          avatar={<Avatar name="John Doe" src="https://i.pravatar.cc/50" />}
+        >
+          <IoLogOutOutline className="w-5 h-5 text-white" />
+        </Chip>
       </Tooltip>
     </section>
   );
