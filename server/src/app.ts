8b2cb3b8cfe63a44a1b0dcfcefa670338243d695
francomattar <mattarfranco3@gmail.com>
2024-06-12 17:03:52 +0600

    integrate root application

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,47 +14,51 @@
  */
 
 /* external imports */
-import express, { Express, Request, Response, NextFunction } from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
+import express, { Express, Request, Response, NextFunction } from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config()
 
 /* internal import */
-dotenv.config();
-import error from "./middleware/error.middleware";
+import error from './middleware/error.middleware'
+import autRouter from './routes/auth.route'
+import otpRouter from './routes/otp.route'
 
 /* application level connection */
-let app: Express = express();
+let app: Express = express()
 
 /* middleware connections */
 app.use(
   cors({
     origin: process.env.ORIGIN_URL,
-    methods: "GET, PATCH, POST, DELETE",
+    methods: 'GET, PUT, POST, DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
-  })
-);
-app.use(express.json());
+  }),
+)
+app.use(express.json())
 
 /* router level connections */
+app.use('/api/auth', autRouter)
+app.use('/api/otp', otpRouter)
 
 /* global error handler */
-app.use(error);
+app.use(error)
 
 /* connection establishment */
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).json({
       acknowledgement: true,
-      message: "OK",
-      description: "The request is OK",
-    });
+      message: 'OK',
+      description: 'The request is OK',
+    })
   } catch (err) {
-    next(err);
+    next(err)
   } finally {
-    console.log(`Route: ${req.url} || Method: ${req.method}`);
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
   }
-});
+})
 
 /* export app */
-export default app;
+export default app
