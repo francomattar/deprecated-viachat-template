9c710a4b0f61d50406fdf86c1d9ac71ee0df1faf
francomattar <mattarfranco3@gmail.com>
2024-06-13 02:32:51 +0600

    initialize global toast message

---
+++ client/hooks/useToastMessage.ts
@@ -0,0 +1,58 @@
+/**
+ * Title: Write a program using JavaScript on UseToastMessage
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 13, June 2024
+ */
+
+"use client";
+
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+interface DataItem {
+  acknowledgement: boolean;
+  description: string;
+}
+
+interface ErrorData {
+  data: {
+    description: string;
+  };
+}
+
+function useToastMessage(
+  data: DataItem | null,
+  error: ErrorData | null,
+  loading: boolean
+) {
+  useEffect(() => {
+    if (loading) {
+      toast.loading("Please wait for the progress", { id: "react-hot-toast" });
+    } else {
+      toast.dismiss("react-hot-toast");
+    }
+
+    if (data) {
+      if (data.acknowledgement) {
+        toast.success(data.description, { id: "react-hot-toast" });
+      } else {
+        toast.error(data.description, { id: "react-hot-toast" });
+      }
+    }
+
+    if (error?.data) {
+      toast.error(error.data.description, { id: "react-hot-toast" });
+    }
+  }, [data, error, loading]);
+}
+
+export default useToastMessage;
